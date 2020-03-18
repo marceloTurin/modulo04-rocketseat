3a39b6986f513ea38dc39f90fee39a70d556fd93
Marcelo <marcelo.sousa1997@hotmail.com>
2020-03-17 21:20:38 -0300

    Propriedades do React

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,6 @@
 import React, { Component} from 'react';
+
+import TechItem from './TechItem';
  
 class TechList extends Component {
     
@@ -33,11 +35,12 @@ class TechList extends Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <ul>
-                    {this.state.techs.map(tech => (
-                        <li key={tech}>
-                            {tech}
-                        <button type="button" onClick={() => this.handleDelete(tech)}>Remover</button>   
-                        </li>
+                    {this.state.techs.map(tech => ( 
+                        <TechItem 
+                            key={tech} 
+                            tech={tech} 
+                            onDelete={() => this.handleDelete(tech)} 
+                        /> 
                     ))}
                 </ul>
                 <input 
